927df31c685229403eb10dd9cce9e8de2fcb9c86
Aleksey <parshikovav@gamil.com>
2022-06-29 19:08:40 +0300

    Fixed dependency problem.

--- product-service/src/main/java/com/nelolik/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/nelolik/productservice/ProductServiceApplication.java
@@ -3,8 +3,9 @@ package com.nelolik.productservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.nelolik.productservice.mapper")
 public class ProductServiceApplication {
 

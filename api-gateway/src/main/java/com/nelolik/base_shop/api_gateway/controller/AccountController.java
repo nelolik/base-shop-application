634a70c19e36b53d0db358cfb1448edac8123c8d
Aleksey <parshikovav@gamil.com>
2022-07-04 21:33:29 +0300

    Created a barebone of a api-gateway service.

---
+++ api-gateway/src/main/java/com/nelolik/base_shop/api_gateway/controller/AccountController.java
@@ -0,0 +1,23 @@
+package com.nelolik.base_shop.api_gateway.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AccountController {
+
+    public List<Object> getListOfOrders() {
+        return null;//TODO
+    }
+
+    public List<Object> getUnfinishedOrders() {
+        return null;//TODO
+    }
+
+    public Integer getBonusesCount() {
+        return null;//TODO
+    }
+
+
+}
---
+++ api-gateway/src/main/java/com/nelolik/base_shop/api_gateway/controller/MainPageController.java
@@ -0,0 +1,44 @@
+package com.nelolik.base_shop.api_gateway.controller;
+
+import com.nelolik.base_shop.api_gateway.model.Product;
+import com.nelolik.base_shop.api_gateway.model.ProductShort;
+import com.nelolik.base_shop.api_gateway.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MainPageController {
+
+    private final ProductService productService;
+
+    @GetMapping("/catalog/names")
+    public List<String> getCatalogItemNames() {
+        return null;//TODO
+    }
+
+    @GetMapping("/advices")
+    public List<ProductShort> getPopularItems() {
+        return null;//TODO
+    }
+
+    @GetMapping("/search/{text}")
+    public List<Product> getSearchedItems() {
+        return null;//TODO
+    }
+
+    @GetMapping("/catalog/category/{name}")
+    public List<ProductShort> getCategoryItems(@PathVariable("name") String name) {
+        return productService.getCategoryProducts(name);
+    }
+
+    @GetMapping("/catalog/id/{id}")
+    public Product getItemById(@PathVariable("id") long id) {
+        return productService.getProductById(id);
+    }
+
+}

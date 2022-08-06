8b3ffe6c5006e320cadae7c526e4945a42f2756e
nelolik <parshikovav@gmail.com>
2022-08-06 19:54:01 +0300

    Added Application file.

--- statistic-service/src/main/java/com/nelolik/base_shop/statistic_service/StatisticServiceApplication.java
+++ statistic-service/src/main/java/com/nelolik/base_shop/statistic_service/StatisticServiceApplication.java
@@ -2,8 +2,9 @@ package com.nelolik.base_shop.statistic_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StatisticServiceApplication {
 
     public static void main(String[] args) {

720d30ec0e326fe3e0761f3ed6e1878e023c8486
nelolik <parshikovav@gmail.com>
2022-08-31 23:48:53 +0300

    Added aspect class for logging in api-gateway.

--- api-gateway/src/main/java/com/nelolik/base_shop/api_gateway/controller/AccountController.java
+++ api-gateway/src/main/java/com/nelolik/base_shop/api_gateway/controller/AccountController.java
@@ -1,12 +1,14 @@
 package com.nelolik.base_shop.api_gateway.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("/account")
 public class AccountController {
 
+    @GetMapping("/account")
     public List<Object> getListOfOrders() {
         throw new UnsupportedOperationException("Not implemented yet");//TODO
     }
---
+++ api-gateway/src/main/java/com/nelolik/base_shop/api_gateway/logging/ControllersLogging.java
@@ -0,0 +1,32 @@
+package com.nelolik.base_shop.api_gateway.logging;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class ControllersLogging {
+
+    @Autowired
+    HttpServletRequest request;
+
+    @Pointcut("execution(* com.nelolik.base_shop.api_gateway.controller.*Controller.*(..))")
+    public void controllerPointcut() {}
+
+    @Before("controllerPointcut()")
+    public void doBeforeRequestHandle(JoinPoint joinPoint) {
+        log.info("\nRequestURI={},Method={},\nParameterMap={}", request.getRequestURI(), request.getMethod(),
+                Arrays.toString(joinPoint.getArgs()));
+    }
+
+}

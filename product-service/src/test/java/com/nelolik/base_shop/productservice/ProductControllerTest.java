441145d49567b04ffb47718366afd35d230ac557
nelolik <parshikovav@gmail.com>
2022-09-11 23:43:09 +0300

    Added missed test for ProductController in product-service.

--- product-service/src/test/java/com/nelolik/base_shop/productservice/ProductControllerTest.java
+++ product-service/src/test/java/com/nelolik/base_shop/productservice/ProductControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
@@ -44,8 +45,13 @@ public class ProductControllerTest {
             BigDecimal.valueOf(80.35), 77, "office");
     private static final Product p3 = new Product(ID++, "beacon", "tasty thing",
             BigDecimal.valueOf(349.99), 7, "food");
-    private static final List<Product> productList = List.of(p1, p2, p3);
+
+    private static final Product p4 = new Product(ID++, "bread", "bakery product obtained by baking dough",
+            BigDecimal.valueOf(65.40), 40, "food");
+    private static final List<Product> productList = List.of(p1, p2, p3, p4);
     private static final List<ProductShort> productShortList = new ArrayList<>();
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeAll
     static void init() {
@@ -62,7 +68,7 @@ public class ProductControllerTest {
         mockMvc.perform(get("/products"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/json")))
-                .andExpect(content().string(containsString(new ObjectMapper().writeValueAsString(productList))));
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(productList))));
 
     }
 
@@ -73,7 +79,7 @@ public class ProductControllerTest {
         mockMvc.perform(get("/products/for_bar"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(content().string(containsString(new ObjectMapper().writeValueAsString(productShortList))));
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(productShortList))));
     }
 
     @Test
@@ -84,10 +90,40 @@ public class ProductControllerTest {
         mockMvc.perform(get("/products/id/" + id))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(content().string(containsString(new ObjectMapper().writeValueAsString(p1))));
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(p1))));
 
         verify(productService, times(1)).getProductById(id);
         verify(statisticService, times(1)).saveProductVisitWithoutUserInfo(p1);
+    }
+
+    @Test
+    void getProductsByCategoryShouldReturnAllProductsFromExistingCategory() throws Exception {
+        String category = "food";
+        List<Product> food = productList.stream().filter(p -> category.equals(p.getCategory())).toList();
+
+        when(productService.getProductsByCategory(category)).thenReturn(food);
+
+        mockMvc.perform(get("/products/category/" + category))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(food))));
+
+        verify(productService, times(1)).getProductsByCategory(category);
+    }
+
+    @Test
+    void getProductsByCategoryShouldReturnEmptyListWhenCategoryIsAbsent() throws Exception {
+        String category = "other";
+        List<Product> expected = Collections.emptyList();
+
+        when(productService.getProductsByCategory(category)).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/products/category/" + category))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(expected))));
+
+        verify(productService, times(1)).getProductsByCategory(anyString());
     }
 
     @Test
@@ -98,7 +134,7 @@ public class ProductControllerTest {
         mockMvc.perform(get("/products/search/" + searchText))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(content().string(containsString(new ObjectMapper().writeValueAsString(productShortList))));
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(productShortList))));
 
     }
 

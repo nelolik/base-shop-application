ac54876d37b639ac67357b339026e705d5bafa9d
nelolik <parshikovav@gmail.com>
2022-09-18 21:31:23 +0300

    Added to git Application class, pom file and application.properties.

--- basket-service/src/main/java/com/nelolik/base_shop/basket_service/BasketServiceApplication.java
+++ basket-service/src/main/java/com/nelolik/base_shop/basket_service/BasketServiceApplication.java
@@ -1,9 +1,10 @@
-package com.nelolik.base_shop;
+package com.nelolik.base_shop.basket_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BasketServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BasketServiceApplication.class, args);
